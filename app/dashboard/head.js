e763df082588df57780b1564789d20d31eceec6d
insectfow <ehdgus441@naver.com>
2023-01-10 17:21:54 +0900

    first update !!

---
+++ app/dashboard/head.js
@@ -0,0 +1,10 @@
+// @next/head의 역할
+export default function Head() {
+	return (
+		<>
+			<title>hi head</title>
+			<meta content="width=device-width, initial-scale=1" name="viewport" />
+			<link rel="icon" href="/favicon.ico" />
+		</>
+	)
+}
